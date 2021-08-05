a52364685b58fab17cf53da8d9c016582b02e4bd
My ACER <My ACER@DESKTOP-R06FUB9>
2021-08-05 19:45:20 -0400

    test

--- Project1/Program.cs
+++ Project1/Program.cs
@@ -6,7 +6,7 @@ namespace Project1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World acer!");
         }
     }
 }
