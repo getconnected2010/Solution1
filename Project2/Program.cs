7a2ac17f7310a8a6fd338818711e7cbf63d874c9
My ACER <My ACER@DESKTOP-R06FUB9>
2021-08-06 14:53:23 -0400

    c#

--- Project2/Program.cs
+++ Project2/Program.cs
@@ -3,10 +3,23 @@ using ClassLibrary1;
 
 namespace Project2
 {
+    public struct myStruct
+    {
+        public int a;
+        int b;
+        public void hi()
+        {
+            Console.WriteLine("hello in struct");
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            myStruct abc = new myStruct();
+            abc.a = 2;
+            abc.hi();
+            //
             int counter = 0;
             double db = 2.0;
             counter = (int) db; //explicit casting into an int
