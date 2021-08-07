db9c5b9830ef7eb78f8b1a9e084e52a3a75d1b88
getconnected2010 <65646133+getconnected2010@users.noreply.github.com>
2021-08-07 13:55:36 -0400

    step

--- Project1/Program.cs
+++ Project1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using ClassLibrary1;
 
 namespace Project1
@@ -7,6 +9,31 @@ namespace Project1
     {
         static void Main(string[] args)
         {
+            int[] arrInt = new int[13];
+            string[] arrStr = new string[2];
+            arrInt[0] = 4;
+            arrStr[0] = "eden";
+
+            ArrayList arrList = new ArrayList();
+            arrList.Add(2);
+            arrList.Add("eden");
+
+            List<int> genIntList = new List<int>();
+            genIntList.Add(1);
+            genIntList.Add(5);
+
+            List<string> genStrList = new List<string>();
+            genStrList.Add("eden");
+            genStrList.Add("daddy");
+            genStrList.Add("mommy");
+            genStrList.Add("grandma");
+            genStrList.Add("mahlet");
+
+            foreach(string item in genStrList)
+            {
+                Console.WriteLine(item);
+            }
+
             Calc x = new Calc();
             x.greet();
             Console.WriteLine(x.add(2, 6));
